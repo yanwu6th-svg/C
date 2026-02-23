e52ed439e91056d61e0de2612c0eadd655716519
yanwu6th-svg <yanwu6th@gmail.com>
2026-02-23 22:44:16 +0800

    Update Hello 演算法 - 鄰接表.cpp

--- Hello 演算法 - 鄰接表.cpp
+++ Hello 演算法 - 鄰接表.cpp
@@ -28,6 +28,7 @@ public:
     }
 
     void Addvertex(Vertex * vet){
+        if(adjlist.count(vet)) return;
         adjlist[vet] = vector<Vertex *>();
     }
 
@@ -66,15 +67,63 @@ public:
         }
 
     }
+
+    vector<Vertex *> BFS(Vertex *vet){
+        vector<Vertex *> res;
+        queue<Vertex *> q;
+        unordered_set<Vertex *> visited;
+        q.push(vet);
+        visited.insert(vet);
+        while(!q.empty()){
+                Vertex * h = q.front();
+                res.push_back(h);
+
+                for(auto v : adjlist[h]){
+                    if(visited.find(v) == visited.end()){
+                        q.push(v);
+                        visited.insert(v);
+                    }
+                }
+                q.pop();
+        }
+        return res;
+    }
+
+     void dfs(vector<Vertex *> &res,unordered_set<Vertex *> &visited,   Vertex * &curr){
+        visited.insert(curr);
+        res.push_back(curr);
+        if(!adjlist[curr].empty()){
+            for(auto i : adjlist[curr]){
+                if(!visited.count(i)){
+                    dfs(res,visited,i);
+                }
+            }
+        }
+        return;
+    }
+
+    vector<Vertex *> DFS(Vertex * start){
+        vector<Vertex *> res;
+        unordered_set<Vertex *> visited;
+        dfs(res,visited,start);
+        return res;
+    }
 };
 
+void vec_print(vector<Vertex *> v){
+    for(auto i : v){
+        cout << i->val <<" ";
+    }
+}
 int main(){
     Vertex *v1 = new Vertex(1);
     Vertex *v2 = new Vertex(2);
     Vertex *v3 = new Vertex(3);
     Vertex *v4 = new Vertex(4);
     Vertex *v5 = new Vertex(5);
-    vector<vector<Vertex *>> edges = {{v1,v3},{v2,v3},{v3,v5},{v2,v1}};
+    vector<vector<Vertex *>> edges = {{v2,v4},{v1,v4},{v1,v3},{v2,v3},{v3,v5},{v2,v1}};
     GraphadjList g(edges);
-    g.print();
+    vector<Vertex *> res = g.DFS(v1);
+    vec_print(res);
+
 }
