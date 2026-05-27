a5f02c14a3ebe3e76d2f893cc0bad8e158f55104
yanwu6th-svg <yanwu6th@gmail.com>
2026-05-27 14:15:51 +0800

    Update APCS201703-3.數字龍捲風.cpp

--- APCS201703-3.數字龍捲風.cpp
+++ APCS201703-3.數字龍捲風.cpp
@@ -6,7 +6,7 @@ int main(){
 	cin >> n;
    int d=0;
    cin >> d;
-   
+
 	vector<vector<int>> nums(n,vector<int> (n));
 	int s=0;
 	for(int i=0;i<n;i++){
@@ -15,24 +15,32 @@ int main(){
 		   nums[i][j] = s;
 	    }
 	}
-	
+
 	int x=n/2;
 	int y=n/2;
-	
+
 	int count=1;
-	vector<int> dx = {-1,0,1,0};
-	vector<int> dy = {0,-1,0,1};
+	vector<int> dx = {0,-1,0,1};
+	vector<int> dy = {-1,0,1,0};
 	int step=1;
-	
+    cout << nums[x][y];
 	while(true){
-		if(count > n*n) break;
-		for(int i=0;i<step;i++){
-			cout << nums[x][y];
+		if(count >= n*n) break;
+		for(int j=0;j<2;j++){
+            for(int i=0;i<step;i++){
 			x += dx[d];
 			y += dy[d];
-			count++;
+
+			if(x>=0&&x<n&&y>=0&&y<n){
+                cout << nums[x][y];
+                count++;
+			}
+
 		}
 		d = (d+1)%4;
+		}
+
+
 		step++;
 	}
 }
