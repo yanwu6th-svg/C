34802eb3ef0f73989453955c40432a1279346175
yanwu6th-svg <yanwu6th@gmail.com>
2026-05-29 07:02:32 +0800

    Update APCS201703-4.基地台.cpp

--- APCS201703-4.基地台.cpp
+++ APCS201703-4.基地台.cpp
@@ -7,10 +7,10 @@ bool j(int D,vector<int> nums,int k){
     int h=0;
     bool f=false;
 	int n = nums.size();
-    while(a<0){
+    while(a>=0){
         int H = nums[h] + D;
         a--;
-        while(nums[h] > H){
+        while(nums[h] <= H){
             h++;
             if(h == n-1){
                 f =true;
@@ -42,6 +42,7 @@ int mid = l + (l-r)/2;
 
 int res = INT_MAX;
 while(r<=l){
+    mid = l + (l-r)/2;
     if(j(mid,nums,k)){
         res = mid;
         l = mid-1;
