b57bd4995c7a3eff331ae8cc3177fbdc3561ab81
yanwu6th-svg <yanwu6th@gmail.com>
2026-05-29 16:09:08 +0800

    Update APCS201703-4.基地台.cpp

--- APCS201703-4.基地台.cpp
+++ APCS201703-4.基地台.cpp
@@ -2,24 +2,15 @@
 using namespace std;
 
 bool j(int D,vector<int> nums,int k){
-    if(D ==1)return true;
-    int a=k;
-    int h=0;
-    bool f=false;
-	int n = nums.size();
-    while(a>=0){
-        int H = nums[h] + D;
-        a--;
-        while(nums[h] <= H){
-            h++;
-            if(h == n-1){
-                f =true;
-                break;
-            }
+    int count  =1;
+    int H=nums[0]+D;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i] > H){
+            count ++;
+            H=nums[i]+D;
         }
-        if(f == true)return true;
     }
-    if(f == false)return false;
+    return count <= k;
 }
 
 
@@ -36,18 +27,18 @@ sort(nums.begin(),nums.end(),[](int a,int b){
      return a<b;
      });
     
-int r =0;
-int l=nums[n-1]-nums[0];
+int l =0;
+int r=nums[n-1]-nums[0];
 int mid = l + (l-r)/2;
 
 int res = INT_MAX;
-while(r<=l){
-    mid = l + (l-r)/2;
+while(r>=l){
+    mid = l + (r-l)/2;
     if(j(mid,nums,k)){
         res = mid;
-        l = mid-1;
+        r = mid-1;
     }else{
-        r = mid +1;
+        l = mid +1;
     }
 }
 cout << res;
