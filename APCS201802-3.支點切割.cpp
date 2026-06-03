4c04c473e253cfe5ae7ccde6dfba457fe8c11408
yanwu6th-svg <yanwu6th@gmail.com>
2026-06-04 06:57:23 +0800

    Update APCS201802-3.支點切割.cpp

--- APCS201802-3.支點切割.cpp
+++ APCS201802-3.支點切割.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 using ll = long long
+
+ll get_cost(int r,int l,int m,vector<int> &nums,vector<int> &S,vector<int> &SA){
+	ll s = S[r] - S[l-1];
+	ll sa = SA[r] - SA[l-1];
+	
+	return llabs(sa-m*s);
+
+}
+
+
 int main(){
 	
 	int n=0;
@@ -22,12 +32,26 @@ int main(){
 		SA[i] = SA[i-1] + nums[i]*i;
 	}
 	
-	vector<vector<int>> dp(k,vector(n,0))
+	vector<vector<int>> dp(k,vector(n,0));
 	
+	vector<vector<int>> best_cost(n,vector(n,0));
 	for(int l=0;l<n;l++){
 		for(int r=l;r<n;r++){
 			ll best =0;
-			for(int i=l;i<r)
+			for(int i=l;i<r;i++){
+				best = max(best,get_cost(r,l,i,nums,S,SA));
+			}
+			best_cost[r][l] = best;
 		}
 	}
+	
+	for(int K=1;K<k;K++){
+		for(int i=1;i<=n;i++){
+			for(int j=0;j<i;j++){
+				dp[k][i] = max(dp[k-1][i],dp[k-1][j-1]+best_cost[j+1][i])
+			}
+		}
+	}
+	
+	cout << dp[k][n];
 }
