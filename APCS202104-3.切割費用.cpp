f0d61560cc589ddad575abae736f478f8075a886
yanwu6th-svg <yanwu6th@gmail.com>
2026-06-24 19:00:52 +0800

    Update APCS202104-3.切割費用.cpp

--- APCS202104-3.切割費用.cpp
+++ APCS202104-3.切割費用.cpp
@@ -3,23 +3,26 @@ using  namespace std;
 
 int main(){
 int l,n;
-cin >> n >> l
+cin >> n >> l;
 vector<pair<int ,int>> nums(n);
 
 int e,r;
 for(int i =0;i<n;i++){
    cin >> r >> e;
-   nums[i] = {e,r}; 
+   nums[i] = {e,r};
 }
 
 sort(nums.begin(),nums.end());
 
 vector<int> a={0,l};
 
-    
+    long long res=0;
 for(int i=0;i<n;i++){
-    insert(a.begin(),a.end(),lower_bound(a.begin(),a.end(),nums[i].second) - a.begin())
-    
+    int it = lower_bound(a.begin(),a.end(),nums[i].second)- a.begin();
+    res += a[it] - a[it-1];
+    a.insert(a.begin() + it,nums[i].second);
+
 }
+cout << res;
 
 }
